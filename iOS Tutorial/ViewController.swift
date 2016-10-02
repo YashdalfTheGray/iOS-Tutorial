94c799f03e792774d554c24af3487545a2888a2f
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-10-02 08:17:34 -0400

    what up dark mode!

--- iOS Tutorial/ViewController.swift
+++ iOS Tutorial/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func enableDarkMode(_ sender: AnyObject) {
+        view.backgroundColor = UIColor.darkGray;
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
